38f0a2393c3bbff2a9aed983e89b026bde952422
wangxing <18912540921@163.com>
2018-03-27 19:11:09 +0800

    except checkFunction

--- js/app.js
+++ js/app.js
@@ -1,46 +1,83 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function(y) {
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/enemy-bug.png';
+  this.x = -50;
+  this.y = y;
+  this.speed = 20 + Math.random() * 80; //Math.random() 生成0～1随机数
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  this.x += this.speed * dt;
+
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  this.sprite = 'images/char-boy.png';
+  this.x = 250;
+  this.y = 350;
+  this.speed = 25;
+};
 
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
+Player.prototype.handleInput = function(allowedKeys) {
+  switch (allowedKeys) {
+    case 'left':
+      this.x -= 30;
+      break;
+    case 'right':
+      this.x += 30;
+      break;
+    case 'up':
+      this.y -= 30;
+      break;
+    case 'down':
+      this.y += 30;
+  }
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+for (var i = 0; i < 6; i++) {
+  //Math.floor()返回一个表示小于或等于指定数字的最大整数的数字。
+  var enemy = new Enemy(Math.floor(Math.random() * 4) * 80);
+  allEnemies.push(enemy);
+};
+
+var player = new Player();
 
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
